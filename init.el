732c0dde19839b325be066b64de0c829aea036c5
bjakushka <bjakushka@gmail.com>
2014-08-03 15:52:55 +0400

    Lua-Mode (for Lua language) was activated and configured.

--- init.el
+++ init.el
@@ -94,3 +94,12 @@
 ;; Auto Pair (plugin) - auto closing pairs (parens, quotes, etc)
 (require 'autopair)
 (autopair-global-mode)
+
+
+
+;;
+;; Lua-Mode - for Lua language
+(require 'lua-mode)
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\*.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
