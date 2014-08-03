209ce11d120e53d9a13b5d5da916c4f701e83690
bjakushka <bjakushka@gmail.com>
2014-08-03 14:45:39 +0400

    Highlight an expression between parens

--- init.el
+++ init.el
@@ -10,3 +10,9 @@
 (setq make-backup-files nil)
 (setq auto-save-list-file-name nil)
 (setq auto-save-default nil)
+
+;; Highlight an expression between parens
+(setq show-paren-style 'expression)
+(setq show-paren-delay 0)
+(show-paren-mode 2)
+
