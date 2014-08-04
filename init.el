b278b81a4a42af9c245db5e40d28ba83113e444d
bjakushka <bjakushka@gmail.com>
2014-08-04 16:57:22 +0400

    Set Emacss frame size and position for Emacs with graphical interface.

--- init.el
+++ init.el
@@ -43,6 +43,10 @@
   ;; Color of highlighted expression
   (set-face-background 'show-paren-match-face "#3a3a3a")
   (set-face-attribute 'show-paren-match nil :weight 'extra-bold)
+
+  ;; Window size and position for emacs with graphical interface
+  (set-frame-position (selected-frame) 0 0)
+  (set-frame-size (selected-frame) 100 25)
 )
 
 
