03b6ffff8dc3799101a71a5c04c92fc1fb65b1fe
bjakushka <bjakushka@gmail.com>
2014-09-19 13:41:42 +0400

    nXhtml mode was added to project. mumamo background color were disabled.

--- init/init-nxhtml.el
+++ init/init-nxhtml.el
@@ -4,6 +4,8 @@
 (add-to-list 'load-path (expand-file-name "plugins/hxtml" user-emacs-directory))
 (load "~/.emacs.d/plugins/nxhtml/autostart.el")
 
+(setq mumamo-background-colors nil)
+
 
 
 (provide 'init-nxhtml)
