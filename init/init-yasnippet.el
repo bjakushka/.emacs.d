2372d1d5e29559b0d12c9dc54a33539a83d98eca
bjakushka <bjakushka@gmail.com>
2014-09-25 01:04:51 +0400

    Adding few new snippets.

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -3,10 +3,15 @@
 ;; URL: https://github.com/capitaomorte/yasnippet
 ;;
 (require 'yasnippet)
+;; use only own snippets
+(setq yas-snippet-dirs (eval (expand-file-name "snippets" user-emacs-directory)))
 (yas-global-mode 1)
 
 ;; Jump to end of snippet definition 
 (define-key yas-keymap (kbd "<return>") 'yas/exit-all-snippets)
+
+;; Wrap around region
+;;(setq yas-wrap-around-region t)
 
 
 
