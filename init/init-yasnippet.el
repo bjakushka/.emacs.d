fc0cb01b91c768b30f2c45bb1b0a37025cbdbc45
bjakushka <bjakushka@gmail.com>
2014-09-24 23:39:11 +0400

    yasnippet directories structure were reorganized.

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -2,9 +2,7 @@
 ;; Yasnippet - template system for Emacs
 ;; URL: https://github.com/capitaomorte/yasnippet
 ;;
-(add-to-list 'load-path (expand-file-name "plugins/yasnippet" user-emacs-directory))
 (require 'yasnippet)
-(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 (yas-global-mode 1)
 
 ;; Jump to end of snippet definition 
