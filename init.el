0efe602c1d2550eba8b95d054e6fdc504362abae
bjakushka <bjakushka@gmail.com>
2014-09-25 03:16:46 +0400

    Zen-Coding was added to project.

--- init.el
+++ init.el
@@ -41,6 +41,7 @@
 (require 'init-mmm)
 (require 'init-text-mode)
 (require 'init-moz-reload-mode)
+(require 'init-zen-coding)
 ;;
 ;; languages
 ;;
---
+++ init/init-zen-coding.el
@@ -0,0 +1,9 @@
+;;-=====================================================
+;; Zen Coding - for writing HTML and CSS quickerly.
+;; 
+(require 'zencoding-mode)
+(add-hook 'web-mode-hook 'zencoding-mode)
+
+
+
+(provide 'init-zen-coding)
