da8758af7db3982ae410bf4fef399b123152732c
Kyrylo Petrov <bjakushka@gmail.com>
2021-09-01 16:07:08 +0300

    Fix soft-wrapping in org-mode

--- init/init-org-mode.el
+++ init/init-org-mode.el
@@ -4,7 +4,9 @@
 (add-to-list 'auto-mode-alist '("\\.org%" . org-mode))
 (setq org-log-done t)
 (add-hook 'org-mode-hook (lambda ()
-			      (auto-fill-mode t)))
+    (auto-fill-mode -1)
+    (global-visual-line-mode t)
+))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 
